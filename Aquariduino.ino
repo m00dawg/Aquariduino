47084db71abf463803cc556a6acbccaa5ec1e3c3
Tim Soderstrom <tim@moocowproductions.org>
2012-10-04 16:23:43 -0500

    Functionalized code

--- Aquariduino.ino
+++ Aquariduino.ino
@@ -1,12 +1,13 @@
 /*
-  Aquariduino v0.20
+  Aquariduino v0.25
+  By: Tim Soderstrom
 
   Pins used:
     Heater Relay: 12
     LCD Shield: Analog Pins 4 & 5 (I2C Bus)
     Temperature 1 Wire Bus: 7
 */
- 
+
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -17,6 +18,12 @@
 //#include <SPI.h>
 //#include <Ethernet.h>
 //#include <WiFi.h>
+
+/*
+ * -------
+ * DEFINES
+ * ------- 
+ */
 
 /* LCD Colors */
 #define RED 0x1
@@ -34,7 +41,13 @@
 /* One wire bus pin */
 #define ONE_WIRE_BUS 7
 
-/* Initialize LCD object */
+/*
+ * -------
+ * OBJECTS
+ * ------- 
+ */
+
+// LCD Object
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
@@ -43,9 +56,15 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
-// Arrays to hold temp devices 
+// Arrays to hold temperature devices 
 // DeviceAddress insideThermometer, outsideThermometer;
 DeviceAddress tankThermometer;
+
+/* 
+ * ---------
+ * CONSTANTS
+ * ---------
+ */
 
 /* Define LCD Size */
 const int lcdColumns = 16;
@@ -57,8 +76,10 @@ const int heaterPin = 12;
 /* 1 second = 1000 milliseconds */
 const int second = 1000;
 
-/* Temp sesnor polling interval (seconds) and display hold time */
-const int pollingInterval = 5;
+/* Polling and update timeouts */
+const int sensorPollingInterval = 5;
+const int lcdUpdateInterval = 5;
+const int alertTimeout = 5;
 
 /* 
    Temperature range to cycle heater in Celsius
@@ -67,8 +88,8 @@ const int pollingInterval = 5;
    alertHighTemp = Tank too hot even with heater off
    alertLowTemp = Tank too cold even with heater on
 */
-const float lowTemp = 24.5;
-const float highTemp = 25.0;
+const float lowTemp = 24.25;
+const float highTemp = 24.5;
 const float alertHighTemp = 27.0;
 const float alertLowTemp = 23.0;
 
@@ -80,7 +101,10 @@ const float alertLowTemp = 23.0;
 
 /* Number of milliseconds since bootup */
 unsigned long currentMillis = 0;
-unsigned long previousMillis = 0;
+
+/* Poll Timeouts for various things */
+unsigned long lastSensorPoll = 0;
+unsigned long lastLCDUpdate = 0;
 
 /* Min and max temperatures seen */
 float maxTemp = 0;
@@ -125,66 +149,25 @@ void loop()
 {
   buttons = lcd.readButtons(); 
 
-  /* Get some temperature readings and do stuff */
   currentMillis = millis();
-    
-  if(currentMillis - previousMillis > pollingInterval * second)
+  
+  /* If it's been longer than the polling interval, poll sensors */
+  if(currentMillis - lastSensorPoll > sensorPollingInterval * second)
   {
-    previousMillis = currentMillis;    
-    sensors.requestTemperatures();
-    if(sensors.getAddress(tankThermometer, 0))
-    {
-      currentTemp = sensors.getTempC(tankThermometer);
-      /* Check to see if we hit a new low or high temp */
-      if(currentTemp > maxTemp)
-        maxTemp = currentTemp;
-      if(currentTemp < minTemp)
-        minTemp = currentTemp;
-      /* If temperature is too high, turn off heater */
-      if(currentTemp > highTemp)
-      {
-        digitalWrite(heaterPin, LOW);
-        heater = false;
-        if(currentTemp > alertHighTemp)
-          backlightColor = RED;
-        else
-          backlightColor = GREEN;
-      }
-      
-      /* If the temperature is too low, turn on heater */
-      else if(currentTemp < lowTemp)
-      {
-        digitalWrite(heaterPin, HIGH);
-        heater = true;
-        if(backlight)
-        {
-          if(currentTemp < alertLowTemp)
-            backlightColor = BLUE;
-          else
-            backlightColor = YELLOW;
-        }
-      }
-      if(heater)
-        displayInfo("Temp: " + String(floatToString(currentTemp)) + "C", "Heater On");
-      else
-        displayInfo("Temp: " + String(floatToString(currentTemp)) + "C", "Heater Off");  
-    }
+    if(collectTemperatures())
+      controlHeater();  
     else
-    {
-      backlightColor = RED;
-      displayInfo("NO TEMP SENSORS", "");
-    }
-    if(backlight)
-      lcd.setBacklight(backlightColor);
+      error("NO SENSORS");
+    lastSensorPoll = currentMillis;
   }
   
   /* Process button input */
   if (buttons)
   {
     if (buttons & BUTTON_LEFT)
-      displayInfo("CFG Temp Range:", String(floatToString(lowTemp)) + "-" + String(floatToString(highTemp)) + " C");
+      displayInfo("Temp Range:", String(floatToString(lowTemp)) + "-" + String(floatToString(highTemp)) + " C");
     if (buttons & BUTTON_RIGHT)
-      displayInfo("Temp Alerts:", String(floatToString(alertLowTemp)) + "-" + String(floatToString(alertHighTemp)) + " C");
+      displayInfo("Alert Temps:", String(floatToString(alertLowTemp)) + "-" + String(floatToString(alertHighTemp)) + " C");
     if (buttons & BUTTON_UP)
       displayInfo("Min/Max Temps:", String(floatToString(minTemp)) + "-" + String(floatToString(maxTemp)) + " C");
     if (buttons & BUTTON_DOWN)
@@ -199,30 +182,55 @@ void loop()
       }
       else
       {
-        lcd.setBacklight(ON);
         lcd.setBacklight(backlightColor);
         backlight = true;
         delay(1 * second);   
       }
     }
-    previousMillis = millis();
+    lastLCDUpdate = millis();
     clearLCD = true;
+  }
+  
+  /* Regular Display Routine */
+  else if(currentMillis - lastLCDUpdate > lcdUpdateInterval * second)
+  {
+    if(backlight)
+    {
+      if(currentTemp > alertHighTemp)
+        lcd.setBacklight(RED);
+      else if(currentTemp >= highTemp)
+        lcd.setBacklight(YELLOW);
+      else if(currentTemp > lowTemp && currentTemp < highTemp)
+        lcd.setBacklight(GREEN);
+      else if(currentTemp <= lowTemp)
+        lcd.setBacklight(VIOLET);
+      else if(currentTemp < alertLowTemp)
+        lcd.setBacklight(BLUE);
+    }   
+    if(heater)
+      displayInfo("Temp: " + String(floatToString(currentTemp)) + " C", "Heater On");
+    else
+      displayInfo("Temp: " + String(floatToString(currentTemp)) + " C", "Heater Off");
+    lastLCDUpdate = millis();
   }
 }
 
 void displayInfo(String topText, String bottomText)
 {  
+  if(clearLCD)
+  {
+    lcd.clear();
+    clearLCD = false;
+  }
   lcd.setCursor(0,0);
   lcd.print(padString(topText));
-//  lcd.print(topText);
   lcd.setCursor(0,1);
-//  lcd.print(bottomText);
   lcd.print(padString(bottomText));
 }
 
 String padString(String value)
 {
-  char padding[value.length()];
+  char padding[lcdColumns - value.length()];
   for(int count = 0; count < sizeof(padding); ++count)
     padding[count] = ' ';
   return String(value) + String(padding);
@@ -230,7 +238,52 @@ String padString(String value)
 
 String floatToString(float value)
 {
-  char result[5];
-  dtostrf(value, 3, 1, result);
+  char result[13];
+  dtostrf(value, 4, 1, result);
   return result;
+}
+
+boolean collectTemperatures()
+{
+  sensors.requestTemperatures();
+  if(sensors.getAddress(tankThermometer, 0))
+  {
+    currentTemp = sensors.getTempC(tankThermometer);
+    /* Check to see if we hit a new low or high temp */
+    if(currentTemp > maxTemp)
+      maxTemp = currentTemp;
+    if(currentTemp < minTemp)
+      minTemp = currentTemp;
+    return true;
+  }
+  return false;
+}
+
+void controlHeater()
+{
+    /* If temperature is too high, turn off heater */
+    if(currentTemp > highTemp)
+    {
+      digitalWrite(heaterPin, LOW);
+      heater = false;
+      return;
+    }
+    /* If the temperature is too low, turn on heater */
+    if(currentTemp < lowTemp)
+    {
+      digitalWrite(heaterPin, HIGH);
+      heater = true;
+    }
+}
+
+void error(String message)
+{
+    displayInfo("**** ERROR ****", message);
+    for(int count = 0; count < alertTimeout; ++count)
+    {
+      lcd.setBacklight(RED);
+      delay(second);
+      lcd.setBacklight(BLUE);
+      delay(second);
+    }
 }
